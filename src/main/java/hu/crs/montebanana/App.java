fbd6ca9d459d91314ac535a7bfa3e543a4f2e212
Viktor Kovacs <crusader8@gmail.com>
2019-05-06 19:56:17 +0200

    make monkey move

--- src/main/java/hu/crs/montebanana/App.java
+++ src/main/java/hu/crs/montebanana/App.java
@@ -1,11 +1,35 @@
 package hu.crs.montebanana;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Arrays.fill;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static void main(String[] args) throws InterruptedException {
+        char[] mountain = new char[13];
+
+        while (true) {
+            int monkeyLocation = ThreadLocalRandom.current().nextInt(13);
+            mountain[monkeyLocation] = '@';
+
+            System.out.println(toString(mountain));
+            System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _");
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println("\033[H\033[2J");
+
+            fill(mountain, ' ');
+        }
+
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    private static String toString(char[] array) {
+        StringBuilder stringBuilder = new StringBuilder(26);
+        for (char c : array) {
+            if (c == ' ') stringBuilder.append("  ");
+            else stringBuilder.append("@ ");
+        }
+        return stringBuilder.toString();
     }
+
 }
