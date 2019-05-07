7d5a51c893ea26f67e6284aa6748582bdcc4ec20
Viktor Kovacs <crusader8@gmail.com>
2019-05-07 22:11:13 +0200

    move arrow

--- src/main/java/hu/crs/montebanana/App.java
+++ src/main/java/hu/crs/montebanana/App.java
@@ -1,33 +1,47 @@
 package hu.crs.montebanana;
 
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
-
-import static java.util.Arrays.fill;
+import java.util.Scanner;
 
 public class App {
+
+    //TODO: terminal raw mode https://www.darkcoding.net/software/non-blocking-console-io-is-not-possible/
     public static void main(String[] args) throws InterruptedException {
         char[] mountain = new char[13];
 
+        int monkeyIndex = 1;
+        Scanner in = new Scanner(System.in);
         while (true) {
-            int monkeyLocation = ThreadLocalRandom.current().nextInt(13);
-            mountain[monkeyLocation] = '@';
-
+            mountain[monkeyIndex] = '@';
             System.out.println(toString(mountain));
             System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _");
-            TimeUnit.SECONDS.sleep(1);
-            System.out.println("\033[H\033[2J");
+            System.out.println(arrow(monkeyIndex));
+            String next = in.next();
 
-            fill(mountain, ' ');
+            mountain[monkeyIndex] = ' ';
+            if ("d".equals(next)) {
+                if (monkeyIndex < 12) monkeyIndex++;
+            } else if ("a".equals(next)) {
+                if (monkeyIndex > 0) monkeyIndex--;
+            }
+            System.out.println("\033[H\033[2J");
         }
 
+    }
+
+    private static String arrow(int monkeyIndex) {
+        if (monkeyIndex == 0) {
+            return "^";
+        } else {
+            return "-".repeat((monkeyIndex * 2) - 1) +"/^";
+
+        }
     }
 
     private static String toString(char[] array) {
         StringBuilder stringBuilder = new StringBuilder(26);
         for (char c : array) {
-            if (c == ' ') stringBuilder.append("  ");
-            else stringBuilder.append("@ ");
+            if (c == '@') stringBuilder.append("@ ");
+            else stringBuilder.append("  ");
         }
         return stringBuilder.toString();
     }
