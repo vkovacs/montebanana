a001fe3d51223c57790a835369093ad80d6a0b45
Viktor Kovacs <crusader8@gmail.com>
2019-05-17 21:36:01 +0200

    add player test

---
+++ src/test/java/hu/crs/montebanana/components/PlayerTest.java
@@ -0,0 +1,38 @@
+package hu.crs.montebanana.components;
+
+import org.junit.Test;
+import tool.Color;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+public class PlayerTest {
+
+    @Test
+    public void removeCard() {
+        Player player = new Player(0, Color.RED);
+        player.removeCard(4);
+
+        assertThat(player.getCards(), containsInAnyOrder(1,2,3,5));
+    }
+
+    @Test
+    public void receiveBanana() {
+        Player player = new Player(0, Color.RED);
+        assertThat(player.getBananas(), is(0));
+
+        player.receiveBanana();
+        assertThat(player.getBananas(), is(1));
+    }
+
+    @Test
+    public void getBackAllCards() {
+        Player player = new Player(0, Color.RED);
+        player.removeCard(1);
+        player.removeCard(5);
+        player.getBackAllCards();
+
+        assertThat(player.getCards(), containsInAnyOrder(1,2,3,4,5));
+    }
+}
