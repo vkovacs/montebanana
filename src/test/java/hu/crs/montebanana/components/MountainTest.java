39fc61a395479f9a05cbe43ddd71791d171a237f
Viktor Kovacs <crusader8@gmail.com>
2019-05-18 09:51:44 +0200

    add test

---
+++ src/test/java/hu/crs/montebanana/components/MountainTest.java
@@ -0,0 +1,42 @@
+package hu.crs.montebanana.components;
+
+import hu.crs.montebanana.movement.IllegalLocationException;
+import hu.crs.montebanana.movement.Movement;
+import org.junit.Test;
+import tool.Color;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static hu.crs.montebanana.movement.Direction.LEFT;
+import static hu.crs.montebanana.movement.Direction.RIGHT;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MountainTest {
+
+    @Test(expected = IllegalLocationException.class)
+    public void findNextEmptyLocationIllegalLeftStepFromStartingPosition() {
+        Player player = new Player(0, Color.RED);
+        Player[] players = new Player[13];
+        Map<Integer, Integer> playerLocation = new HashMap<>();
+
+        playerLocation.put(0, -1);
+        Mountain mountain = new Mountain(players, playerLocation);
+
+        mountain.step(player, new Movement(1, LEFT));
+    }
+
+    @Test
+    public void findNextEmptyLocationLegalStepFromStartingPosition() {
+        Player player = new Player(0, Color.RED);
+        Player[] players = new Player[13];
+        Map<Integer, Integer> playerLocation = new HashMap<>();
+
+        playerLocation.put(0, -1);
+        Mountain mountain = new Mountain(players, playerLocation);
+
+        mountain.step(player, new Movement(2, RIGHT));
+        assertThat(mountain.playerLocation.get(player.getId()), is(1));
+    }
+}
