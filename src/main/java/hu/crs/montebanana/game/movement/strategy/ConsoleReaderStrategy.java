ca9c34f379767f84e1d938475246a973bfaa4871
Viktor Kovacs <crusader8@gmail.com>
2020-08-27 20:58:57 +0200

    make console reader strategy use step validation done by the board

--- src/main/java/hu/crs/montebanana/game/movement/strategy/ConsoleReaderStrategy.java
+++ src/main/java/hu/crs/montebanana/game/movement/strategy/ConsoleReaderStrategy.java
@@ -5,10 +5,9 @@ import hu.crs.montebanana.game.components.Board;
 import hu.crs.montebanana.game.movement.Direction;
 import hu.crs.montebanana.game.movement.Movement;
 import hu.crs.montebanana.game.player.Player;
-import hu.crs.montebanana.game.rendering.Label;
 import hu.crs.montebanana.game.rendering.Color;
+import hu.crs.montebanana.game.rendering.Label;
 
-import java.util.Collection;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -18,21 +17,25 @@ import static hu.crs.montebanana.game.MonteBanana.RENDERER_VISITOR;
 public class ConsoleReaderStrategy implements MovementStrategy {
     public Movement next(Board board, Player player) {
         Scanner in = new Scanner(System.in);
-        return readMovement(in, player.getCards());
+        return readMovement(in, board, player);
     }
 
-    private Movement readMovement(Scanner in, Collection<Integer> availableCards) {
+    private Movement readMovement(Scanner in, Board board, Player player) {
         int stepCount;
         Direction stepDirection;
         while (true) {
             try {
                 String command = in.nextLine();
                 stepCount = Math.abs(Integer.parseInt(command.split(" ")[0]));
-                if (!availableCards.contains(stepCount)) {
-                    throw new IllegalArgumentException("Step is not available!");
-                }
                 stepDirection = Direction.valueOfChar(Character.toLowerCase(command.split(" ")[1].charAt(0)));
-                return new Movement(stepCount, stepDirection);
+
+
+                Movement movement = new Movement(stepCount, stepDirection);
+                boolean validMovement = board.isValidMovement(player, movement);
+                if (validMovement) {
+                    return movement;
+                }
+
             } catch (IllegalArgumentException | ArrayIndexOutOfBoundsException e) {
                 System.out.println(error("Incorrect input! Try again!"));
             } catch (NoSuchElementException e) {
